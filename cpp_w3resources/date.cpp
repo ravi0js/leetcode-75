dc2debe1816dcb3b2abdd33cc994eb3e334da788
ravi0js <ravi.jsdeveloper@gmail.com>
2025-03-28 18:32:10 +0530

    date format

---
+++ cpp_w3resources/date.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime> 
+using namespace std;
+// 1. Write a C++ program to get the current date and time.
+/*
+int main()
+{
+    time_t current_dt = time(0);  // `time_t` is a data type to store time values
+                                  // `time(0)` returns the current system time in seconds 
+                                  // since the epoch (Jan 1, 1970, UTC)
+
+    char* result = ctime(&current_dt); // `ctime` converts the time (in seconds) to a 
+                                       // human-readable string format (e.g., "Fri Mar 28 14:35:10 2025\n")
+
+    cout << "The current time: " << result;  // Display the formatted current date and time
+
+    return 0; // Exit the program
+}
+
+// 2. Write a C++ program to get the day of the week from a given date. Return the day as a string. The format of the day is MM/DD/YYYY.
+*/
+
+// 3. Write a C++ program to convert a given year to a century.
+// 4. Write a C++ program to check whether a given year is a leap year or not.
+
+// 5. Write a C++ program to compute the number of days in a month for a given year and month.
+
+// 6. Write a C++ program to count the number of days between two given dates.
+
